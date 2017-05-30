9cb98a0c8213f23d3d772475bdebc3bef4b06508
MpTech6 <odoodevelopers6@gmail.com>
2017-05-30 09:10:57 +0530

    [Add & Change]: Added module sale_purchase_changes for changes in tree view.

---
+++ sale_purchase_changes/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Sale/Purchase Changes',
+    'version': '1.0',
+    'summary': 'Sale/Purchase Changes',
+    'description': """
+        Sale/Purchase Changes
+    """,
+    'author': 'HashMicro / Bharat Chauhan',
+    'website': 'www.hashmicro.com',
+    'category': 'Sale / Purchase',
+    'depends': ['sale', 'purchase'],
+    'data': [
+        'views/purchase_view.xml',
+        'views/sale_view.xml',
+    ],
+    'installable': True,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
