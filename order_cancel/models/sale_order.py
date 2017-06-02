9dbc4410791bfcdc825232820b9e91148ed9852b
Hoang Le <hoang@vieterp.net>
2017-06-02 16:24:45 +0700

    Fixed order_cancel.

--- order_cancel/models/sale_order.py
+++ order_cancel/models/sale_order.py
@@ -15,43 +15,57 @@ class SaleOrder(models.Model):
             order_name = order.name.split('-')[0]
             for picking in order.picking_ids:
                 picking_type_ids = picking_type_obj.search(cr, uid, [('name', '=', 'Receipts')], limit=1)
-                for picking_type_id in picking_type_ids:
-                    ## TODO: Create incomming shipment
-                    picking_obj.copy(cr, uid, picking.id, default={
-                        'picking_type_id': picking_type_id,
-                        'state': 'confirmed',
+                if picking.state == 'done':
+                    for picking_type_id in picking_type_ids:
+                        ## TODO: Create incomming shipment
+                        picking_obj.copy(cr, uid, picking.id, default={
+                            'picking_type_id': picking_type_id,
+                            'state': 'confirmed',
+                        }, context=context)
+
+
+
+                    new_version = order.version + 1
+                    self.write(cr, uid, order.id, {
+                        'name': order_name,
+                        'state': 'cancel',
                     }, context=context)
 
-            new_version = order.version + 1
-            self.write(cr, uid, order.id, {
-                'name': '%s-%s' % (order_name, str(new_version)),
-                'state': 'cancel',
-            }, context=context)
+                    order_id = self.copy(cr, uid, order.id, default={
+                        'name': '%s-%s' % (order_name, str(new_version)),
+                        'version': new_version,
+                    }, context=context)
+                    # self.write(cr, uid, order_id, {
+                    #     'name': order_name,
+                    #     'version': new_version
+                    # }, context=context)
 
-            order_id = self.copy(cr, uid, order.id, default={
-                'name': order_name,
-                'version': new_version,
-            }, context=context)
-            # self.write(cr, uid, order_id, {
-            #     'name': order_name,
-            #     'version': new_version
-            # }, context=context)
+                    return {
+                        'name': _("Sale Order"),
+                        'view_mode': 'form',
+                        'view_id': False,
+                        'view_type': 'form',
+                        'res_model': 'sale.order',
+                        'res_id': order_id,
+                        'type': 'ir.actions.act_window',
+                        'nodestroy': True,
+                        'target': 'current',
+                        'domain': '[]',
+                        'context': context
+                    }
+                else:
+                    return {
+                        self.action_cancel(cr, uid, order.id, context=context),
+                        self.write(cr, uid, order.id, {
+                            'name': order_name,
+                            'state': 'draft',
+                        }, context=context)
 
-            return {
-                'name': _("Sale Order"),
-                'view_mode': 'form',
-                'view_id': False,
-                'view_type': 'form',
-                'res_model': 'sale.order',
-                'res_id': order_id,
-                'type': 'ir.actions.act_window',
-                'nodestroy': True,
-                'target': 'current',
-                'domain': '[]',
-                'context': context
-            }
+                    }
+
         return True
         # result = self.action_cancel(cr, uid, ids, context=context)
         # if result:
         #     self.write(cr, uid, ids, {'state': 'draft'}, context=context)
         # return result
+
