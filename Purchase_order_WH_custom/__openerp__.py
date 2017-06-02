131aa5a63e6df5ad1ec8b3090e050d2d741ccdf7
MpTech6 <odoodevelopers6@gmail.com>
2017-06-02 15:42:38 +0530

    [ADD & CHANGE: Filter records on Purchase receipt form for accounts, | Put warehouse_id Many2one field on Purchase Order Line.]

---
+++ Purchase_order_WH_custom/__openerp__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Purchase Order WH",
+
+    'summary': """
+        source warehouse field in the order line in purchase""",
+
+    'description': """
+        This Module contains all the custom fields required for Interflux.
+    """,
+
+    'author': "Techspawn Solution",
+    'website': "http://www.techspawn.com",
+
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    'depends': ['base','purchase'],
+
+    'data': [
+        
+        'templates.xml',
+    ],
+}
---
+++ account_filter_on_purchase_receipt/models/account_voucher.py
@@ -0,0 +1,21 @@
+from openerp import models, api, _
+from openerp.exceptions import Warning
+from openerp import api, fields, models, tools
+
+
+class AccountVoucher(models.Model):
+
+	_inherit = 'account.voucher'
+
+
+	@api.multi
+	def _compute_partner_id(self):
+		asset_type = self.env['ir.model.data'].get_object_reference('account', 'data_account_type_asset')[1]
+		asset_view_type = self.env['ir.model.data'].get_object_reference('account', 'account_type_asset_view1')[1]
+		liability_type = self.env['ir.model.data'].get_object_reference('account', 'data_account_type_liability')[1]
+		liability_view_type = self.env['ir.model.data'].get_object_reference('account', 'account_type_liability_view1')[1]
+
+		account_ids = self.env['account.account'].search([('user_type', 'in', [asset_type, asset_view_type, liability_type, liability_view_type])])
+		return [('id', 'in', account_ids.ids)]
+
+	account_id = fields.Many2one('account.account', 'Account', required=True, domain=_compute_partner_id)
