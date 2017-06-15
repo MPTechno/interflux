6af4e3ab41c4645143fa234f477838e42bdbc5ad
Anand Patel <flt.anandkumar@gmail.com>
2017-06-15 15:20:48 +0530

    [ADD] Delivery Address in Invoice report.

--- interflux_layouts/account_invoice.py
+++ interflux_layouts/account_invoice.py
@@ -38,6 +38,27 @@ class account_invoice(models.Model):
 					if sale_obj:
 						return sale_obj.client_order_ref
         return ''
+
+    #Used for "Invoice" report
+    @api.model
+    def get_delivery_address(self, obj):
+    	sale_order_pool = self.env['sale.order']
+    	stock_picking_pool = self.env['stock.picking']
+
+    	if obj.origin:
+			sale_obj = sale_order_pool.search([('name','=',obj.origin)])
+			if sale_obj:
+				print"sale_obj.deli_address",sale_obj.deli_address
+				return sale_obj.deli_address
+			else:
+				stock_picking_obj = stock_picking_pool.search([('name','=',obj.origin)])
+				if stock_picking_obj:
+					picking_origin = stock_picking_obj.origin
+					sale_obj = sale_order_pool.search([('name','=',picking_origin)])
+					if sale_obj:
+						print"sale_obj.deli_address",sale_obj.deli_address
+						return sale_obj.deli_address
+        return ''
         
     #Used for "Invoice" report
     @api.model
