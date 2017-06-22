96cf118feac68c74837758fcb740b4fdd9b7ce17
Hoang Le <hoang@vieterp.net>
2017-06-22 14:00:11 +0700

    Update change appear of Customer Refund.

--- interflux_layouts/models.py
+++ interflux_layouts/models.py
@@ -179,7 +179,7 @@ class res_partner_bank(osv.osv):
 	def name_search(self, cr, uid, name, args=None, operator='ilike', context=None, limit=100):
 		ids = self.search(cr, uid, args, context=context)
 
-		if not len(ids) > 0:
+		if args == [['id', 'in', []]]:
 			ids = self.search(cr, uid, [], context=context)
 		return self.name_get(cr, uid, ids, context=context)
 
